312251409111664341da0e70e91915e438cac065
tonysinghmss <manishankarsngh007@gmail.com>
2017-11-06 00:22:18 +0530

    Minor refactoring

--- app/src/main/java/com/tony/odiya/mahanadi/adapter/MyExpenseRecyclerViewAdapter.java
+++ app/src/main/java/com/tony/odiya/mahanadi/adapter/MyExpenseRecyclerViewAdapter.java
@@ -4,13 +4,12 @@ import android.graphics.Color;
 import android.support.v7.util.SortedList;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.tony.odiya.mahanadi.R;
+import com.tony.odiya.mahanadi.adapter.vholder.ExpenseViewHolder;
 import com.tony.odiya.mahanadi.fragment.ExpenseFragment.OnListFragmentInteractionListener;
 import com.tony.odiya.mahanadi.model.ExpenseData;
 
@@ -23,7 +22,7 @@ import java.util.List;
  * {@link RecyclerView.Adapter} that can display a {@link ExpenseData} and makes a call to the
  * specified {@link OnListFragmentInteractionListener}.
  */
-public class MyExpenseRecyclerViewAdapter extends SelectableAdapter<MyExpenseRecyclerViewAdapter.ViewHolder> {
+public class MyExpenseRecyclerViewAdapter extends SelectableAdapter<ExpenseViewHolder> {
     private static final String LOG_TAG = MyExpenseRecyclerViewAdapter.class.getSimpleName();
     private final List<ExpenseData> mValues;
     private final OnListFragmentInteractionListener mContextListener;
@@ -31,10 +30,9 @@ public class MyExpenseRecyclerViewAdapter extends SelectableAdapter<MyExpenseRec
     private final List<String> mExpenseDataIdList = new ArrayList<>(0);
     private Double totalExpenseAmount = 0.0;
 
-    private Comparator<ExpenseData> mComparator;
-
     public interface OnRecyclerItemClickedListener {
         void onItemClicked(int position);
+
         boolean onItemLongClicked(int position);
         //int updateCurrentMonthBudgetRow(List<String> expenseIdList);
     }
@@ -43,204 +41,35 @@ public class MyExpenseRecyclerViewAdapter extends SelectableAdapter<MyExpenseRec
         mValues = items;
         mContextListener = listener;
         mItemLongClickedListener = itemLongClickedListener;
-        //mComparator = ALPHABETICAL_COMPARATOR;
     }
-/*
-
-    public void setComparator(Comparator<ExpenseData>  comparator){
-        mComparator = comparator;
-    }
-
-    public static final Comparator<ExpenseData> ALPHABETICAL_COMPARATOR = new Comparator<ExpenseData>() {
-        @Override
-        public int compare(ExpenseData a, ExpenseData b) {
-            return a.getItem().compareTo(b.getItem());
-        }
-    };
-    *//*
-
-    private final SortedList<ExpenseData> mSortedList = new SortedList<>(ExpenseData.class, new SortedList.Callback<ExpenseData>() {
-        @Override
-        public int compare(ExpenseData a, ExpenseData b) {
-            return mComparator.compare(a, b);
-        }
-
-        @Override
-        public void onInserted(int position, int count) {
-            notifyItemRangeInserted(position, count);
-        }
-
-        @Override
-        public void onRemoved(int position, int count) {
-            notifyItemRangeRemoved(position, count);
-        }
-
-        @Override
-        public void onMoved(int fromPosition, int toPosition) {
-            notifyItemMoved(fromPosition, toPosition);
-        }
-
-        @Override
-        public void onChanged(int position, int count) {
-            notifyItemRangeChanged(position, count);
-        }
-
-        @Override
-        public boolean areContentsTheSame(ExpenseData oldItem, ExpenseData newItem) {
-            return oldItem.equals(newItem);
-        }
-
-        @Override
-        public boolean areItemsTheSame(ExpenseData item1, ExpenseData item2) {
-            return item1.getExpenseId().equals(item2.getExpenseId());
-        }
-    });
-
-    public void add(ExpenseData model) {
-        mSortedList.add(model);
-    }
-
-    public void remove(ExpenseData model) {
-        mSortedList.remove(model);
-    }
-
-    public void add(List<ExpenseData> models) {
-        mSortedList.addAll(models);
-    }
-
-    public void remove(List<ExpenseData> models) {
-        mSortedList.beginBatchedUpdates();
-        for (ExpenseData model : models) {
-            mSortedList.remove(model);
-        }
-        mSortedList.endBatchedUpdates();
-    }
-
-    public void replaceAll(List<ExpenseData> models) {
-        mSortedList.beginBatchedUpdates();
-        for (int i = mSortedList.size() - 1; i >= 0; i--) {
-            final ExpenseData model = mSortedList.get(i);
-            if (!models.contains(model)) {
-                mSortedList.remove(model);
-            }
-        }
-        mSortedList.addAll(models);
-        mSortedList.endBatchedUpdates();
-    }
-
-*/
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ExpenseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //Take viewtype and change the color of views in future
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.expense_card, parent, false);
-        return new ViewHolder(view, mItemLongClickedListener);
+        return new ExpenseViewHolder(view, mItemLongClickedListener);
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, final int position) {
-        holder.mItem = mValues.get(position);
-        holder.mCategoryView.setText(mValues.get(position).category);
-        holder.mItemView.setText(mValues.get(position).item);
-        holder.mAmountView.setText(mValues.get(position).amount);
-        holder.mRemarkView.setText(mValues.get(position).remark);
-        CardView v= (CardView)holder.mView.findViewById(R.id.expense_card_view);
-        if(isSelected(position)){
-            v.setCardBackgroundColor(Color.parseColor("#ff0099cc"));
-            //holder.mView.setBackgroundColor(Color.parseColor("#ff0099cc"));
-            holder.mCategoryView.setTextColor(Color.WHITE);
-            holder.mItemView.setTextColor(Color.WHITE);
-            holder.mAmountView.setTextColor(Color.WHITE);
-            holder.mRemarkView.setTextColor(Color.WHITE);
-        }
-        else {
+    public void onBindViewHolder(final ExpenseViewHolder holder, final int position) {
+        holder.bindTo(mValues.get(position));
+        CardView v = (CardView) holder.mView.findViewById(R.id.expense_card_view);
+        if (isSelected(position)) {
+            v.setCardBackgroundColor(Color.parseColor("#ff0099cc"));//Dark blue
+            holder.setViewTextColor(Color.WHITE);
+        } else {
             v.setCardBackgroundColor(Color.WHITE);
-            /*View v= holder.mView.findViewById(R.id.expense_card_view);
-            v.setBackgroundColor(Color.TRANSPARENT);*/
-            //holder.mView.setBackgroundColor(Color.TRANSPARENT);
-            holder.mCategoryView.setTextColor(Color.BLACK);
-            holder.mItemView.setTextColor(Color.BLACK);
-            holder.mAmountView.setTextColor(Color.BLACK);
-            holder.mRemarkView.setTextColor(Color.BLACK);
+            holder.setViewTextColor(Color.BLACK);
         }
-        //holder.mSelectedOverlay.setVisibility(isSelected(position)?View.VISIBLE:View.INVISIBLE);
-        /*View card = holder.mView.findViewById(R.id.expense_card_view);
-        card.setSelected(isSelected(position));*/
-
-       /* holder.mView.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                 if (null != mContextListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mContextListener.onListFragmentInteraction(holder.mItem);
-                }
-            }
-        });
-        holder.mView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                mItemLongClickedListener.onItemLongClicked(position);
-                return true;
-            }
-        });*/
-
     }
 
     @Override
     public int getItemCount() {
-        if(mValues!=null) {
+        if (mValues != null) {
             return mValues.size();
-        }
-        else
+        } else
             return 0;
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
-        public final View mView;
-        public final TextView mCategoryView;
-        public final TextView mItemView;
-        public final TextView mAmountView;
-        public final TextView mRemarkView;
-        public final View mSelectedOverlay;
-        public ExpenseData mItem;
-        private OnRecyclerItemClickedListener recyclerItemClickedListener;
-        public ViewHolder(View view, OnRecyclerItemClickedListener recyclerItemClickedListener) {
-            super(view);
-            mView = view;            mCategoryView = (TextView) view.findViewById(R.id.category);
-            mItemView = (TextView) view.findViewById(R.id.item);
-            mAmountView = (TextView) view.findViewById(R.id.amount);
-            mRemarkView = (TextView) view.findViewById(R.id.remark);
-            mSelectedOverlay = (View) view.findViewById(R.id.selected_overlay);
-            this.recyclerItemClickedListener = recyclerItemClickedListener;
-            view.setOnClickListener(this);
-            view.setOnLongClickListener(this);
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + " '" + mCategoryView.getText()+", "+mItemView.getText()+", " +mAmountView.getText()+ "'";
-        }
-
-        @Override
-        public void onClick(View v){
-            Log.d(LOG_TAG, "You have clicked on "+mItemView.getText());
-            int position = getAdapterPosition();
-            if(recyclerItemClickedListener !=null){
-                recyclerItemClickedListener.onItemClicked(position);
-            }
-        }
-
-        @Override
-        public boolean onLongClick(View v){
-            Log.d(LOG_TAG, "You have long clicked on "+mItemView.getText());
-            int position = getAdapterPosition();
-            if(recyclerItemClickedListener !=null){
-                return recyclerItemClickedListener.onItemLongClicked(position);
-            }
-            return false;
-        }
     }
 
     public void removeItem(int position) {
@@ -298,11 +127,11 @@ public class MyExpenseRecyclerViewAdapter extends SelectableAdapter<MyExpenseRec
     /*public int updateCurrentMonthBudgetRow(){
         return mItemLongClickedListener.updateCurrentMonthBudgetRow(mExpenseDataIdList);
     }*/
-    public List<String> getExpenseDataIdList(){
+    public List<String> getExpenseDataIdList() {
         return mExpenseDataIdList;
     }
 
-    public Double getTotalExpenseAmount(){
+    public Double getTotalExpenseAmount() {
         return totalExpenseAmount;
     }
 
